e9a36f946f3c8aca4187afbe7a75aefae1027802
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-14 09:39:22 +0700

    feat/user : register route user to server

--- src/applications/server.ts
+++ src/applications/server.ts
@@ -1,4 +1,8 @@
 import express from "express";
+import { publicRouter } from "../routes/public-api";
+import { errorMiddleware } from "../middlewares/error-middleware";
 
 export const server=express();
 server.use(express.json());
+server.use(publicRouter);
+server.use(errorMiddleware);
