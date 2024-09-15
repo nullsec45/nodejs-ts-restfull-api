f30255aed6a945b9f5de07ad1aa9f0d3c06bde1f
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-15 14:08:51 +0700

    feat/user : register, login, get.

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,12 +1,48 @@
 import { Request, Response, NextFunction } from "express";
-import { CreateUserRequest } from "../models/user-model";
+import { CreateUserRequest, LoginUserRequest, UpdateUserRequest } from "../models/user-model";
 import { UserService } from "../services/user-service";
+import { UserRequest } from "../types/user-request";
 
 export class UserController{
     static async register(req:Request, res:Response, next:NextFunction){
         try{
             const request:CreateUserRequest=req.body as CreateUserRequest;
             const response=await UserService.register(request);
+            res.status(200).json({
+                data: response
+            })
+        }catch(e){
+            next(e);
+        }
+    }
+
+    static async login(req:Request, res:Response, next:NextFunction){
+        try{
+            const request:LoginUserRequest=req.body as LoginUserRequest;
+            const response=await UserService.login(request);
+            res.status(200).json({
+                data:response
+            });
+        }catch(e){
+            next(e);
+        }
+    }
+
+     static async get(req:UserRequest, res:Response, next:NextFunction){
+        try{
+            const response=await UserService.get(req.user!);
+            res.status(200).json({
+                data:response
+            });
+        }catch(e){
+            next(e);
+        }
+    }
+
+    static async update(req:UserRequest, res:Response, next:NextFunction){
+        try{
+            const request: UpdateUserRequest = req.body as UpdateUserRequest;
+            const response = await UserService.update(req.user!, request);
             res.status(200).json({
                 data: response
             })
