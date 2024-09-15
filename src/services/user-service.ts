c3ef143347feb225509fb5c4b880ff45a730cfcc
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-15 14:12:16 +0700

    feat/user : service login, get, update.

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,9 +1,11 @@
 import { prismaClient } from "../applications/database";
 import { ResponseError } from "../errors/response-error";
-import { CreateUserRequest, toUserResponse, UserResponse } from "../models/user-model";
+import { CreateUserRequest, LoginUserRequest, toUserResponse, UpdateUserRequest, UserResponse } from "../models/user-model";
 import { UserValidation } from "../validations/user-validation";
 import { Validation } from "../validations/validation";
 import bcrypt from "bcrypt";
+import {v4 as uuid} from "uuid";
+import { User } from "@prisma/client";
 
 export class UserService{
     static async register(request:CreateUserRequest):Promise<UserResponse>{
@@ -26,5 +28,63 @@ export class UserService{
         });
 
         return toUserResponse(user);
+    }
+
+    static async login(request:LoginUserRequest) : Promise<UserResponse>{
+        const loginRequest=Validation.validate(UserValidation.LOGIN, request);
+
+        let user=await prismaClient.user.findUnique({
+            where:{
+                username:loginRequest.username
+            }
+        });
+
+        if(!user){
+            throw new ResponseError(401,"Username or password is wrong");
+        }
+
+        const isPasswordValid=await bcrypt.compare(loginRequest.password, user.password);
+
+        if(!isPasswordValid){
+            throw new ResponseError(401,"Username or password is wrong");
+        }
+
+          user = await prismaClient.user.update({
+            where: {
+                username: loginRequest.username
+            },
+            data: {
+                token: uuid()
+            }
+        });
+
+        const response=toUserResponse(user);
+        response.token=user.token!;
+        return response;
+    }
+    
+    static async get(user:User):Promise<UserResponse>{
+        return toUserResponse(user);
+    }
+
+    static async update(user:User, request:UpdateUserRequest):Promise<UserResponse>{
+        const updateRequest=Validation.validate(UserValidation.UPDATE, request);
+
+        if(updateRequest.name){
+            user.name=updateRequest.name;
+        }
+
+        if(updateRequest.password){
+            user.password=await bcrypt.hash(updateRequest.password, 10);
+        }
+
+        const result=await prismaClient.user.update({
+            where:{
+                username:user.username
+            },
+            data:user
+        });
+
+        return toUserResponse(result);
     }
 }
