a65adced51ea7094fe7372b0dad2318b8aed6dfc
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-15 14:07:28 +0700

    feat/user : register api user to server

--- src/applications/server.ts
+++ src/applications/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { publicRouter } from "../routes/public-api";
 import { errorMiddleware } from "../middlewares/error-middleware";
+import { apiRouter } from "../routes/api";
 
 export const server=express();
 server.use(express.json());
 server.use(publicRouter);
+server.use(apiRouter);
 server.use(errorMiddleware);
