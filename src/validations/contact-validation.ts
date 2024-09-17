ccbd4a4a178e44d9f206548447e6ad888033b7b3
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-17 11:46:22 +0700

    feat/contact : new contact validation.

---
+++ src/validations/contact-validation.ts
@@ -0,0 +1,21 @@
+import {z, ZodType} from "zod";
+
+
+export class ContactValidation{
+    static readonly CREATE:ZodType=z.object({
+        first_name:z.string().min(1).max(100),
+        last_name:z.string().min(1).max(100).optional(),
+        email:z.string().min(1).max(100).email().optional(),
+        phone:z.string().min(1).max(20).optional()
+    });
+
+     static readonly LOGIN:ZodType=z.object({
+        username:z.string().min(1).max(100),
+        password:z.string().min(1).max(100),
+    });
+
+    static  readonly UPDATE:ZodType=z.object({
+        name:z.string().min(1).max(100).optional(),
+        password:z.string().min(1).max(100).optional(),
+    });
+}
