e749f59f87206d5a084a0e879d2f261290d0fa6d
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-17 11:45:36 +0700

    feat/contact : add route contacts.

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth-middleware";
 import { UserController } from "../controllers/user-controller";
+import { ContactController } from "../controllers/contact-controller";
 
 export const apiRouter=express.Router();
 apiRouter.use(authMiddleware);
 
-
-// user api
+// User API
 apiRouter.get("/api/users/current", UserController.get);
 apiRouter.patch("/api/users/current", UserController.update);
+apiRouter.delete("/api/users/current", UserController.logout);
+
+// Contact API
+apiRouter.post("/api/contacts",ContactController.create);
