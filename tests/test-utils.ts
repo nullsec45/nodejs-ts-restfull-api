f06559b52915e974ccebb8d9ac7f53e1ceb98f82
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-20 01:32:37 +0700

    test/contact: get & create contact utils.

--- tests/test-utils.ts
+++ tests/test-utils.ts
@@ -1,6 +1,6 @@
 import { prismaClient } from "../src/applications/database";
 import bcrypt from "bcrypt";
-import { User } from "@prisma/client";
+import { User, Contact } from "@prisma/client";
 
 export class UserTest{
     static async delete(){
@@ -40,5 +40,31 @@ export class ContactTest{
                 username:"test"
             }
         });
+    }
+
+    static async create(){
+        await prismaClient.contact.create({
+            data:{
+                first_name:"test",
+                last_name:"test",
+                email:"test@gmail.com",
+                phone: "0812345678",
+                username:"test"
+            }
+        });
+    }
+
+    static async get():Promise<Contact>{
+        const contact=await prismaClient.contact.findFirst({
+            where:{
+                username:"test"
+            }
+        });
+
+        if(!contact){
+            throw new Error("Contact is not found");
+        }
+
+        return contact;
     }
 }
