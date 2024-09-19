552fb739e1e301d128405d7cecc195cb4662bb9b
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-20 01:28:48 +0700

    feat/contact : get & update contact controller.

--- src/controllers/contact-controller.ts
+++ src/controllers/contact-controller.ts
@@ -1,6 +1,6 @@
 import { UserRequest } from "../types/user-request";
 import {Response,NextFunction} from "express";
-import { CreateContactRequest } from "../models/contact-model";
+import { CreateContactRequest, UpdataContactRequest } from "../models/contact-model";
 import { ContactService } from "../services/contact-service";
 import { logger } from "../applications/logger";
 
@@ -9,6 +9,35 @@ export class ContactController{
         try{
             const request:CreateContactRequest=req.body as CreateContactRequest;
             const response=await ContactService.create(req.user!, request);
+
+            res.status(200).json({
+                data:response
+            })
+        }catch(e){
+            next(e);
+        }
+    }
+
+     static async get(req:UserRequest, res:Response, next:NextFunction){
+        try{
+            const contactId=Number(req.params.contactId);
+            const response=await ContactService.get(req.user!, contactId);
+
+            res.status(200).json({
+                data:response
+            })
+        }catch(e){
+            next(e);
+        }
+    }
+
+     static async update(req:UserRequest, res:Response, next:NextFunction){
+        try{
+            const request:UpdataContactRequest=req.body as UpdataContactRequest;
+            request.id=Number(req.params.contactId);
+            const response=await ContactService.update(req.user!, request);
+
+            logger.debug("response : "+JSON.stringify(response));
 
             res.status(200).json({
                 data:response
