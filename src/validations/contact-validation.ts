32c1da4ac39a0785a309079b0d6b65f1b99aa289
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-20 01:31:27 +0700

    feat/contact : add column validation update contact.

--- src/validations/contact-validation.ts
+++ src/validations/contact-validation.ts
@@ -15,7 +15,10 @@ export class ContactValidation{
     });
 
     static  readonly UPDATE:ZodType=z.object({
-        name:z.string().min(1).max(100).optional(),
-        password:z.string().min(1).max(100).optional(),
+        id:z.number().positive(),
+        first_name:z.string().min(1).max(100).optional(),
+        last_name:z.string().min(1).max(100).optional(),
+        email:z.string().min(1).max(100).email().optional(),
+        phone:z.string().min(1).max(20).optional()
     });
 }
