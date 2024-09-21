53f0f0fbb25bc547d6f04f55f0803ab97705c172
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-21 21:48:31 +0700

    feat/contact : remove & search service.

--- src/services/contact-service.ts
+++ src/services/contact-service.ts
@@ -1,5 +1,6 @@
 import { ContactResponse, 
     CreateContactRequest, 
+    SearchContactRequest, 
     toContactResponse, 
     UpdateContactRequest 
 } from "../models/contact-model";
@@ -10,6 +11,7 @@ import { prismaClient } from "../applications/database";
 import { logger } from "../applications/logger";
 import { ResponseError } from "../errors/response-error";
 import { UserValidation } from "../validations/user-validation";
+import { Pageable } from "../types/page";
 
 export class ContactService{
      static async create(user: User, request: CreateContactRequest): Promise<ContactResponse> {
@@ -30,7 +32,7 @@ export class ContactService{
     }
 
     static  async checkContactMustExists(username:string, contactId:number){
-        const contact=await prismaClient.contact.findUnique({
+        const contact=await prismaClient.contact.findFirst({
             where:{
                 id:contactId,
                 username:username
@@ -51,7 +53,7 @@ export class ContactService{
     }
 
     static async update(user:User, request:UpdateContactRequest):Promise<ContactResponse>{
-        const updateRequest=Validation.validate(UserValidation.UPDATE, request);
+        const updateRequest=Validation.validate(ContactValidation.UPDATE, request);
         await this.checkContactMustExists(user.username, updateRequest.id);
 
         const contact=await prismaClient.contact.update({
@@ -63,5 +65,83 @@ export class ContactService{
         });
 
         return toContactResponse(contact);
+    }
+
+    static async remove(user:User, id:number):Promise<ContactResponse>{
+        await this.checkContactMustExists(user.username, id);
+
+        const contact=await prismaClient.contact.delete({
+            where:{
+                id:id,
+                username:user.username
+            }
+        });
+
+        return toContactResponse(contact);
+    }
+
+    static async search(user:User, request:SearchContactRequest):Promise<Pageable<ContactResponse>>{
+        const searchRequest=Validation.validate(ContactValidation.SEARCH, request);
+        const skip=(searchRequest.page - 1) * searchRequest.size;
+
+        const filters=[];
+        // check if name exists;
+        if(searchRequest.name){
+            filters.push({
+                OR:[
+                    {
+                        first_name:{
+                            contains:searchRequest.name
+                        }
+                    },
+                    {
+                        last_name:{
+                            contains:searchRequest.name
+                        }
+                    }
+                ]
+            });
+        }
+        // check if email exists;
+        if(searchRequest.email){
+            filters.push({
+               email:{
+                contains:searchRequest.email
+               }
+            });
+        }
+        // check if phone exists;
+         if(searchRequest.phone){
+            filters.push({
+               phone:{
+                contains:searchRequest.phone
+               }
+            });
+        }
+
+        const contacts=await prismaClient.contact.findMany({
+            where:{
+                username:user.username,
+                AND:filters
+            },
+            take:searchRequest.size,
+            skip:skip
+        });
+
+        const total=await prismaClient.contact.count({
+            where:{
+                username:user.username,
+                AND:filters
+            }
+        });
+
+        return {
+            data:contacts.map(contact => toContactResponse(contact)),
+            paging:{
+                current_page:searchRequest.page,
+                total_page:Math.ceil(total / searchRequest.size),
+                size:searchRequest.size
+            }
+        }
     }
 }
