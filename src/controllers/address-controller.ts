840b81f976191a0d1c12c8cb769d22b6dea06be6
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-25 23:21:26 +0700

    feat/address : get controller.

--- src/controllers/address-controller.ts
+++ src/controllers/address-controller.ts
@@ -1,7 +1,7 @@
 import { UserRequest } from "../types/user-request";
 import { Response, NextFunction } from "express";
 import { AddressService  } from "../services/address-service";
-import { CreateAddressRequest } from "../models/address-model";
+import { CreateAddressRequest, GetAddressRequest } from "../models/address-model";
 
 export class AddressController{
     static async create(req:UserRequest, res:Response, next:NextFunction){
@@ -9,6 +9,25 @@ export class AddressController{
             const request:CreateAddressRequest=req.body as  CreateAddressRequest;
             request.contact_id = Number(req.params.contactId);
             const response=await AddressService.create(req.user!, request);
+
+            res.status(200).json({
+                data:response
+            })
+        }catch(e){
+            next(e);
+        }
+    }
+
+    static async get(req:UserRequest, res:Response, next:NextFunction){
+        try{
+            const request:GetAddressRequest={
+                id:Number(req.params.addressId),
+                contact_id:Number(req.params.contactId),
+                
+            }
+
+            request.contact_id = Number(req.params.contactId);
+            const response=await AddressService.get(req.user!, request);
 
             res.status(200).json({
                 data:response
