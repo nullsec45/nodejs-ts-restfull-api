d1fb9297de4a4dfd632fea6e5bbd6ebcec13bc83
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-25 23:22:56 +0700

    feat/address : get service.

--- src/services/address-service.ts
+++ src/services/address-service.ts
@@ -2,6 +2,7 @@ import {
     AddressResponse, 
     CreateAddressRequest, 
     toAddressResponse, 
+    GetAddressRequest
 } from "../models/address-model";
 import { Validation } from "../validations/validation";
 import {AddressValidation}  from "../validations/address-validation";
@@ -10,8 +11,24 @@ import { prismaClient } from "../applications/database";
 import { logger } from "../applications/logger";
 import { ResponseError } from "../errors/response-error";
 import { ContactService } from "./contact-service";
+import { add } from "winston";
 
 export class AddressService{
+    static  async checkContactMustExists(username:string, contactId:number){
+        const contact=await prismaClient.contact.findFirst({
+            where:{
+                id:contactId,
+                username:username
+            }
+        });
+
+         if(!contact){
+            throw new ResponseError(404, "Contact Not Found");
+        }
+
+        return contact;
+    }
+    
     static async create(user: User, request: CreateAddressRequest): Promise<AddressResponse> {
         const createRequest = Validation.validate(AddressValidation.CREATE, request);
         await ContactService.checkContactMustExists(user.username, request.contact_id);
@@ -26,18 +43,21 @@ export class AddressService{
         return toAddressResponse(address);
     }
 
-    static  async checkContactMustExists(username:string, contactId:number){
-        const contact=await prismaClient.contact.findFirst({
+    static async get(user:User, request:GetAddressRequest):Promise<AddressResponse>{
+        const getRequest=Validation.validate(AddressValidation.GET, request);
+        await ContactService.checkContactMustExists(user.username, request.contact_id);
+
+        const address=await prismaClient.address.findFirst({
             where:{
-                id:contactId,
-                username:username
+                id:getRequest.id,
+                contact_id:getRequest.contact_id,
             }
         });
 
-         if(!contact){
-            throw new ResponseError(404, "Contact Not Found");
+        if(!address){
+            throw new ResponseError(404, "Address is not found");
         }
 
-        return contact;
+        return toAddressResponse(address);
     }
 }
